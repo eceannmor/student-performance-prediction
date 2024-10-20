22e64085e13e600f81228a1eaae926edad93c6e8
Ekre Ceannmor <avolkodavdm@gmail.com>
2024-10-20 21:26:33 +0200

    [database] » created init and import scripts for the database

--- PRO1D/src/app/DataService.java
+++ PRO1D/src/app/DataService.java
@@ -1,17 +1,29 @@
 package app;
+
 import java.sql.*;
 
 public class DataService {
     public static Connection open(UrlAndCredentials urlAndCredentials) {
         String databaseUrl = "jdbc:postgresql://" + urlAndCredentials.hostname() +
-        ":" + urlAndCredentials.port() + "/"
-        + urlAndCredentials.dbname();
+                ":" + urlAndCredentials.port() + "/"
+                + urlAndCredentials.dbname();
         try {
-            Connection connection = DriverManager.getConnection(databaseUrl, urlAndCredentials.username(), urlAndCredentials.password());
+            Connection connection = DriverManager.getConnection(databaseUrl, urlAndCredentials.username(),
+                    urlAndCredentials.password());
             return connection;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static void exitNicely(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
         }
     }
 }
