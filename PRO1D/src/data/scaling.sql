4b64bb3b86d1c1e2b8ad46b2ec23e6f765529a85
Oleksandr Volkodav <ovolkodav@vertabelo.com>
2024-11-03 21:29:58 +0100

    [data-standardisation] » query troubleshooting, minor changes

--- PRO1D/src/data/scaling.sql
+++ PRO1D/src/data/scaling.sql
@@ -7,151 +7,85 @@
 
 -- Yes this is human written, yes this is how I talk.
 
--- HoursStudied is an uncapped non-negative integer. Scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- HoursStudied is an uncapped non-negative integer. Normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS HoursStudiedMinMax;
+ADD COLUMN IF NOT EXISTS HoursStudiedMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.HoursStudiedMinMax IS 'HoursStudied, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS HoursStudiedZ;
+ADD COLUMN IF NOT EXISTS HoursStudiedZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.HoursStudiedZ IS 'HoursStudied, normalised using Z-Score normalisation.';
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (HoursStudied - MIN(HoursStudied) OVER ())::numeric / 
-       (MAX(HoursStudied) OVER () - MIN(HoursStudied) OVER ()) AS HoursStudiedMinMax,
-       (HoursStudied - AVG(HoursStudied) OVER())::numeric / STDDEV(HoursStudied) OVER() AS HoursStudiedZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.HoursStudiedMinMax = tmp.HoursStudiedMinMax
-SET spf.HoursStudiedZ = tmp.HoursStudiedZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET HoursStudiedMinMax = (HoursStudied - (SELECT MIN(HoursStudied) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(HoursStudied) FROM StudentPerformanceFactors)) - (SELECT MIN(HoursStudied) FROM StudentPerformanceFactors),
+    HoursStudiedZ = (HoursStudied - (SELECT AVG(HoursStudied) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(HoursStudied) FROM StudentPerformanceFactors);
 
--- Attendance is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- Attendance is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS AttendanceMinMax;
+ADD COLUMN IF NOT EXISTS AttendanceMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.AttendanceMinMax IS 'Attendance, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS AttendanceZ;
+ADD COLUMN IF NOT EXISTS AttendanceZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.AttendanceZ IS 'Attendance, normalised using Z-Score normalisation.';
 UPDATE StudentPerformanceFactors
 SET Attendance = Attendance / 100.0
 WHERE 1 > (SELECT AVG(Attendance) FROM StudentPerformanceFactors);
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (Attendance - MIN(Attendance) OVER ())::numeric / 
-       (MAX(Attendance) OVER () - MIN(Attendance) OVER ()) AS AttendanceMinMax,
-       (Attendance - AVG(Attendance) OVER())::numeric / STDDEV(Attendance) OVER() AS AttendanceZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.AttendanceMinMax = tmp.AttendanceMinMax
-SET spf.AttendanceZ = tmp.AttendanceZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET AttendanceMinMax = (Attendance - (SELECT MIN(Attendance) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(Attendance) FROM StudentPerformanceFactors)) - (SELECT MIN(Attendance) FROM StudentPerformanceFactors),
+    AttendanceZ = (Attendance - (SELECT AVG(Attendance) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(Attendance) FROM StudentPerformanceFactors);
 
--- SleepHours is an uncapped non-negative integer. Scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- SleepHours is an uncapped non-negative integer. Normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS SleepHoursMinMax;
+ADD COLUMN IF NOT EXISTS SleepHoursMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.SleepHoursMinMax IS 'SleepHours, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS SleepHoursZ;
+ADD COLUMN IF NOT EXISTS SleepHoursZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.SleepHoursZ IS 'SleepHours, normalised using Z-Score normalisation.';
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (SleepHours - MIN(SleepHours) OVER ())::numeric / 
-       (MAX(SleepHours) OVER () - MIN(SleepHours) OVER ()) AS SleepHoursMinMax,
-       (SleepHours - AVG(SleepHours) OVER())::numeric / STDDEV(SleepHours) OVER() AS SleepHoursZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.SleepHoursMinMax = tmp.SleepHoursMinMax
-SET spf.SleepHoursZ = tmp.SleepHoursZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET SleepHoursMinMax = (SleepHours - (SELECT MIN(SleepHours) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(SleepHours) FROM StudentPerformanceFactors)) - (SELECT MIN(SleepHours) FROM StudentPerformanceFactors),
+    SleepHoursZ = (SleepHours - (SELECT AVG(SleepHours) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(SleepHours) FROM StudentPerformanceFactors);
 
--- PreviousScores is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- PreviousScores is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS PreviousScoresMinMax;
+ADD COLUMN IF NOT EXISTS PreviousScoresMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.PreviousScoresMinMax IS 'PreviousScores, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS PreviousScoresZ;
+ADD COLUMN IF NOT EXISTS PreviousScoresZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.PreviousScoresZ IS 'PreviousScores, normalised using Z-Score normalisation.';
 UPDATE StudentPerformanceFactors
 SET PreviousScores = PreviousScores / 100.0
 WHERE 1 > (SELECT AVG(PreviousScores) FROM StudentPerformanceFactors);
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (PreviousScores - MIN(PreviousScores) OVER ())::numeric / 
-       (MAX(PreviousScores) OVER () - MIN(PreviousScores) OVER ()) AS PreviousScoresMinMax,
-       (PreviousScores - AVG(PreviousScores) OVER())::numeric / STDDEV(PreviousScores) OVER() AS PreviousScoresZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.PreviousScoresMinMax = tmp.PreviousScoresMinMax
-SET spf.PreviousScoresZ = tmp.PreviousScoresZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET PreviousScoresMinMax = (PreviousScores - (SELECT MIN(PreviousScores) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(PreviousScores) FROM StudentPerformanceFactors)) - (SELECT MIN(PreviousScores) FROM StudentPerformanceFactors),
+    PreviousScoresZ = (PreviousScores - (SELECT AVG(PreviousScores) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(PreviousScores) FROM StudentPerformanceFactors);
 
--- TutoringSessions is an uncapped non-negative integer. Scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- TutoringSessions is an uncapped non-negative integer. Normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS TutoringSessionsMinMax;
+ADD COLUMN IF NOT EXISTS TutoringSessionsMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.TutoringSessionsMinMax IS 'TutoringSessions, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS TutoringSessionsZ;
+ADD COLUMN IF NOT EXISTS TutoringSessionsZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.TutoringSessionsZ IS 'TutoringSessions, normalised using Z-Score normalisation.';
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (TutoringSessions - MIN(TutoringSessions) OVER ())::numeric / 
-       (MAX(TutoringSessions) OVER () - MIN(TutoringSessions) OVER ()) AS TutoringSessionsMinMax,
-       (TutoringSessions - AVG(TutoringSessions) OVER())::numeric / STDDEV(TutoringSessions) OVER() AS TutoringSessionsZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.TutoringSessionsMinMax = tmp.TutoringSessionsMinMax
-SET spf.TutoringSessionsZ = tmp.TutoringSessionsZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET TutoringSessionsMinMax = (TutoringSessions - (SELECT MIN(TutoringSessions) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(TutoringSessions) FROM StudentPerformanceFactors)) - (SELECT MIN(TutoringSessions) FROM StudentPerformanceFactors),
+    TutoringSessionsZ = (TutoringSessions - (SELECT AVG(TutoringSessions) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(TutoringSessions) FROM StudentPerformanceFactors);
 
--- PhysicalActivity is an uncapped non-negative integer. Scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- PhysicalActivity is an uncapped non-negative integer. Normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS PhysicalActivityMinMax;
+ADD COLUMN IF NOT EXISTS PhysicalActivityMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.PhysicalActivityMinMax IS 'PhysicalActivity, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS PhysicalActivityZ;
+ADD COLUMN IF NOT EXISTS PhysicalActivityZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.PhysicalActivityZ IS 'PhysicalActivity, normalised using Z-Score normalisation.';
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (PhysicalActivity - MIN(PhysicalActivity) OVER ())::numeric / 
-       (MAX(PhysicalActivity) OVER () - MIN(PhysicalActivity) OVER ()) AS PhysicalActivityMinMax,
-       (PhysicalActivity - AVG(PhysicalActivity) OVER())::numeric / STDDEV(PhysicalActivity) OVER() AS PhysicalActivityZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.PhysicalActivityMinMax = tmp.PhysicalActivityMinMax
-SET spf.PhysicalActivityZ = tmp.PhysicalActivityZ
-WHERE spf.RecordId = Scaling.RecordId;
+UPDATE StudentPerformanceFactors
+SET PhysicalActivityMinMax = (PhysicalActivity - (SELECT MIN(PhysicalActivity) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(PhysicalActivity) FROM StudentPerformanceFactors)) - (SELECT MIN(PhysicalActivity) FROM StudentPerformanceFactors),
+    PhysicalActivityZ = (PhysicalActivity - (SELECT AVG(PhysicalActivity) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(PhysicalActivity) FROM StudentPerformanceFactors);
 
--- ExamScore is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, scaling to [0.0 - 1.0] with min-max, and with z-score.
+-- ExamScore is a non-negative integer, capped at 100. Updating the original data to be capped at 1 instead, normalising with min-max and z-score.
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS ExamScoreMinMax;
+ADD COLUMN IF NOT EXISTS ExamScoreMinMax numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.ExamScoreMinMax IS 'ExamScore, normalised to 0-1 range using min-max normalisation.';
 ALTER TABLE StudentPerformanceFactors 
-ADD COLUMN IF NOT EXISTS ExamScoreZ;
+ADD COLUMN IF NOT EXISTS ExamScoreZ numeric;
 COMMENT ON COLUMN StudentPerformanceFactors.ExamScoreZ IS 'ExamScore, normalised using Z-Score normalisation.';
 UPDATE StudentPerformanceFactors
-SET ExamScore = ExamScore / 100.0
-WHERE 1 > (SELECT AVG(ExamScore) FROM StudentPerformanceFactors);
-WITH Scaling AS (
-    SELECT
-        RecordId,
-       (ExamScore - MIN(ExamScore) OVER ())::numeric / 
-       (MAX(ExamScore) OVER () - MIN(ExamScore) OVER ()) AS ExamScoreMinMax,
-       (ExamScore - AVG(ExamScore) OVER())::numeric / STDDEV(ExamScore) OVER() AS ExamScoreZ
-    FROM StudentPerformanceFactors
-)
-UPDATE StudentPerformanceFactors spf
-SET spf.ExamScoreMinMax = tmp.ExamScoreMinMax
-SET spf.ExamScoreZ = tmp.ExamScoreZ
-WHERE spf.RecordId = Scaling.RecordId;
+SET ExamScoreMinMax = (ExamScore - (SELECT MIN(ExamScore) FROM StudentPerformanceFactors))::numeric / ((SELECT MAX(ExamScore) FROM StudentPerformanceFactors)) - (SELECT MIN(ExamScore) FROM StudentPerformanceFactors),
+    ExamScoreZ = (ExamScore - (SELECT AVG(ExamScore) FROM StudentPerformanceFactors))::numeric / (SELECT STDDEV(ExamScore) FROM StudentPerformanceFactors);
