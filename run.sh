33dbd007b14487e02a85ffd57bbff38255e84af9
Oleksandr Volkodav <ovolkodav@vertabelo.com>
2024-11-18 20:40:48 +0100

    edited run.sh to skip the database part, as the data output is deterministic and is already computed

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-(cd PRO1D/src/app; javac *.java;) 
-(cd PRO1D/src/; java -classpath ../lib/postgresql-42.7.4.jar:. App.java;)
+# (cd PRO1D/src/app; javac *.java;) 
+# (cd PRO1D/src/; java -classpath ../lib/postgresql-42.7.4.jar:. App.java;)
 (cd PRO1D/src/; python3 ml.py;)
 echo Press any key to exit
 read -n 1 -s
